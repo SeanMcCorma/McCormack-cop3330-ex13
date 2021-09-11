472b0da3f512ad9fe376a056c097213b7a924077
Seanfrogs <55803970+Seanfrogs@users.noreply.github.com>
2021-09-10 23:53:23 -0400

    Initial commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("What is the principal amount? ");
+        double initial = Double.parseDouble(scan.nextLine());
+        System.out.printf("What is the rate? ");
+        double rate = Double.parseDouble(scan.nextLine());
+        System.out.printf("What is the number of years? ");
+        int years = Integer.parseInt(scan.nextLine());
+        System.out.printf("What is the number of times the interest is compounded per year? ");
+        int time_year = Integer.parseInt(scan.nextLine());
+        double amount = Math.ceil(initial*Math.pow(1+((rate/100)/time_year),(time_year*years))*100)/100;
+        System.out.printf("$%.2f invested at %.2f%% for %d years compounded %d times per year is $%.2f",initial,rate,years,time_year,amount);
+
+	// write your code here
+    }
+}
